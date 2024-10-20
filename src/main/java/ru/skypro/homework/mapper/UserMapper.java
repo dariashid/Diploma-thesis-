828920180d7f81382c54226acbc0b0e2324c7d53
lvarezhkal <kkakxaa@gmail.com>
2024-10-20 22:11:05 +0300

    Добавил импорты

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import org.apache.catalina.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+import ru.skypro.homework.dto.users.UpdateUserDto;
+import ru.skypro.homework.dto.users.UpdateUserImageDto;
+import ru.skypro.homework.dto.users.UserSetPasswordDto;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
